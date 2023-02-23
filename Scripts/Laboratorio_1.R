cbd114ae64cd984f43af9b77d04fcf85e198671a
Marco UANL <marco.gonzaleztg@uanl.edu.mx>
2023-02-23 08:06:44 -0600

    Crear lab 1

---
+++ Scripts/Laboratorio_1.R
@@ -0,0 +1,14 @@
+# Marco Aurelio González Tagle
+# 23/02/2023
+# Laboratorio 1
+
+# Gastos totales
+
+300 + 240 + 1527 + 400 + 1500 + 1833
+300/6
+200 * 2
+4 ^ 2
+
+celular <- 300
+celular *12
+celular * celular
