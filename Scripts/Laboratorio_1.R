a821f9cb38ee67b5f6a8425c2f21a06776cd3fca
Marco UANL <marco.gonzaleztg@uanl.edu.mx>
2023-02-23 08:44:30 -0600

    agregar instrucciones

--- Scripts/Laboratorio_1.R
+++ Scripts/Laboratorio_1.R
@@ -15,10 +15,15 @@ celular *12
 
 gastos <- c(300, 240, 1527, 400, 1500, 1833)
 gastos
+nomb <- c("celular", "transporte", "comestibles", "gimnasio",
+          "alquiler", "otros")
 
 
 barplot(gastos, 
         col = c("lightgreen",  "red", "lightgreen", "lightgreen","lightgreen","lightgreen"),
         main = "Gastos universitarios mensuales",
         ylim = c(0,2000), xlab = "Rubros",
-        ylab = "Costos en MXP")
+        ylab = "Costos en MXP",
+        names.arg = c("celular", "transporte", "comestibles", "gimnasio",
+                      "alquiler", "otros"),
+        )
