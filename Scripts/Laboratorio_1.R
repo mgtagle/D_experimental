d06f10408637e21b1df5445e1ea25385bd76c9aa
Marco UANL <marco.gonzaleztg@uanl.edu.mx>
2023-02-23 08:36:08 -0600

    add barplot

--- Scripts/Laboratorio_1.R
+++ Scripts/Laboratorio_1.R
@@ -11,4 +11,14 @@
 
 celular <- 300
 celular *12
-celular * celular
+
+
+gastos <- c(300, 240, 1527, 400, 1500, 1833)
+gastos
+
+
+barplot(gastos, 
+        col = c("lightgreen",  "red", "lightgreen", "lightgreen","lightgreen","lightgreen"),
+        main = "Gastos universitarios mensuales",
+        ylim = c(0,2000), xlab = "Rubros",
+        ylab = "Costos en MXP")
