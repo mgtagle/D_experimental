c86652198b0a59a826c71dff3ced7d0402346a12
Marco UANL <marco.gonzaleztg@uanl.edu.mx>
2023-05-11 09:33:45 -0600

    update t.test paired

--- Scripts/Pruebas_t.R
+++ Scripts/Pruebas_t.R
@@ -6,6 +6,7 @@
 # Importar ----------------------------------------------------------------
 
 setwd("C:/Repositorio_GIT/D_experimental/Scripts/")
+
 plantulas <- read.csv("BD1_vivero.csv", header =T)
 plantulas$Tratamiento <- as.factor(plantulas$Tratamiento)
 
@@ -25,3 +26,23 @@ t.test(plantulas$IE ~ plantulas$Tratamiento, var.equal=T)
 mean(plantulas$IE)
 t.test(plantulas$IE, mu = 0.90)
 t.test(plantulas$IE, mu = 0.85)
+
+
+# t-test dependientes ------------------------------------------------------
+
+semilla <- read.csv("mainproduccion.csv", header = T)
+semilla$Tiempo <- as.factor(semilla$Tiempo)
+
+
+# Gráfica boxplot
+
+boxplot(semilla$Tiempo, semilla$Kgsem)
+boxplot(semilla$Tiempo, semilla$BioRama)
+boxplot(semilla$Kgsem)
+
+
+t.test(semilla$Kgsem ~ semilla$Tiempo, paired = TRUE)
+
+t.test(semilla$Germ ~ semilla$Tiempo, paired =T)
+
+t.test(semilla$BioRama ~ semilla$Tiempo, paired=T)
